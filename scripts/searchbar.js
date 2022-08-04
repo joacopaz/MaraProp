4294348f0ba9548dfa15d589d2a58076dccf5995
joacopaz <joacopaz@gmail.com>
2022-08-04 18:06:53 -0300

    updated responsiveness

--- scripts/searchbar.js
+++ scripts/searchbar.js
@@ -1,38 +1,41 @@
-const sb = document.getElementById('search-bar')
-const sbContent = document.querySelector('#search-bar input')
+const sb = document.getElementById("search-bar");
+const sbContent = document.querySelector("#search-bar input");
 
-let expanded = 0
-let animation = false
+let expanded = 0;
+let animation = false;
 
-const wait = ms => new Promise(r => setTimeout(r, ms));
+const wait = (ms) => new Promise((r) => setTimeout(r, ms));
 
 const searchbar = async () => {
-    sb.style.display = 'flex'
-    await wait(50)
-    if (animation) return false
-    if (!expanded) return show();
-    retract()
-}
+	sb.style.display = "flex";
+	await wait(50);
+	if (animation) return false;
+	if (!expanded) return show();
+	retract();
+};
 const show = async () => {
-    animation = true
-    sbContent.value = ''
-    sbContent.style.opacity = '0'
-    sb.style.display = 'flex';
-    await wait(1)
-    sb.style.width = '150px'
-    sbContent.style.opacity = '100'
-    await wait(1000)
-    expanded = 1
-    animation = false
-}
+	animation = true;
+	sbContent.value = "";
+	sbContent.style.opacity = "0";
+	sb.style.display = "flex";
+	await wait(1);
+	sb.style.width = "150px";
+	sbContent.style.opacity = "100";
+	await wait(1000);
+	expanded = 1;
+	animation = false;
+};
 
 const retract = async () => {
-    animation = true
-    if (typeof sbContent.value !== 'undefined' && sbContent.value.length > 0) console.log(`Value is ${sbContent.value}`)
-    sb.style.width = '1px'
-    sbContent.style.opacity = '0';
-    expanded = 0
-    await wait(1000)
-    sb.style.display = ''
-    animation = false
-}
+	animation = true;
+	if (typeof sbContent.value !== "undefined" && sbContent.value.length > 0) {
+		sessionStorage.setItem("search", sbContent.value);
+		return (location.href = "./search.html");
+	}
+	sb.style.width = "1px";
+	sbContent.style.opacity = "0";
+	expanded = 0;
+	await wait(1000);
+	sb.style.display = "";
+	animation = false;
+};
